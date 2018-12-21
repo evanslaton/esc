4994a2d30a8d3bc143b1a703e675ac22d3309651
Evan Slaton <evanslaton@gmail.com>
2018-12-21 14:14:02 -0800

    Added scaffold

---
+++ src/main/java/com/esc/esc/EscApplication.java
@@ -0,0 +1,14 @@
+package com.esc.esc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/esc/esc/EscApplicationTests.java
@@ -0,0 +1,17 @@
+package com.esc.esc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
