c7bb5d5936d622e39c756388d031d1d071cccc06
Daniel Logerstedt <dl@Daniels-MBP.home>
2018-12-22 20:53:58 -0800

    fixed bug in ApplicationUser and fragments.html files, fixed tests

--- src/test/java/com/esc/user/UserControllerTest.java
+++ src/test/java/com/esc/user/UserControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 
@@ -22,39 +24,87 @@ public class UserControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private static String username = "";
+    private static String password = "";
+
+    public void createRandomUserPass() {
+        Random random = new Random();
+        username = "";
+        password = "";
+        for(int i = 0; i < 10; i++) {
+            int randomInt = 97 + (int)
+                    (random.nextFloat() * (26));
+            username += (char)randomInt;
+            password += (char)randomInt;
+        }
+    }
+
     @Test
     public void testServeHomePage() {
-        EscIntegrationSuite testSuite = new EscIntegrationSuite(port, restTemplate);
-        testSuite.createEntity();
-        testSuite.request("GET", "/");
-        assertEquals(200, testSuite.responseCodeInt());
-        assertTrue(testSuite.responseBody().contains("<h1>SPLASH PAGE</h1>"));
+        EscIntegrationSuite testHome = new EscIntegrationSuite(port, restTemplate);
+        testHome.createEntity();
+        testHome.request("GET", "/");
+        assertEquals(200, testHome.responseCodeInt());
+        assertTrue(testHome.responseBody().contains("<h1>SPLASH PAGE</h1>"));
     }
 
     @Test
     public void testServeSignUpPage() {
-        EscIntegrationSuite testSuite = new EscIntegrationSuite(port, restTemplate);
-        testSuite.createEntity();
-        testSuite.request("GET", "/signup");
-        assertEquals(200, testSuite.responseCodeInt());
-        assertTrue(testSuite.responseBody().contains("<h1>SIGNUP PAGE</h1>"));
+        EscIntegrationSuite testSignup = new EscIntegrationSuite(port, restTemplate);
+        testSignup.createEntity();
+        testSignup.request("GET", "/signup");
+        assertEquals(200, testSignup.responseCodeInt());
+        assertTrue(testSignup.responseBody().contains("<h1>SIGNUP PAGE</h1>"));
     }
 
     @Test
     public void testCreateNewUser() {
-        EscIntegrationSuite testSuite = new EscIntegrationSuite(port, restTemplate);
-        testSuite.setBody("username", "testuser");
-        testSuite.setBody("password", "testpass");
-        testSuite.setBody("phoneNumber", "555-555-5555");
-        testSuite.createEntity();
-        testSuite.request("POST", "/signup");
-        assertEquals(302, testSuite.responseCodeInt());
-        assertTrue(testSuite.responseCookieIndex(0).contains("JSESSIONID"));
+        createRandomUserPass();
+        EscIntegrationSuite testNewUser = new EscIntegrationSuite(port, restTemplate);
+        testNewUser.setBody("username", username);
+        testNewUser.setBody("password", password);
+        testNewUser.setBody("phoneNumber", "555-555-5555");
+        testNewUser.createEntity();
+        testNewUser.request("POST", "/signup");
+        assertEquals(302, testNewUser.responseCodeInt());
+        assertTrue(testNewUser.responseCookieIndex(0).contains("JSESSIONID"));
+        testNewUser.request("GET", "/perform_logout");
     }
 
-//    @Test
-//    public void testServeProfilePage() {
-//
-//
-//    }
+    @Test
+    public void testLogin() {
+        createRandomUserPass();
+        EscIntegrationSuite testNewUser = new EscIntegrationSuite(port, restTemplate);
+        testNewUser.setBody("username", username);
+        testNewUser.setBody("password", password);
+        testNewUser.setBody("phoneNumber", "555-555-5555");
+        testNewUser.createEntity();
+        testNewUser.request("POST", "/signup");
+        EscIntegrationSuite testLogin = new EscIntegrationSuite(port, restTemplate);
+        testLogin.setBody("username", username);
+        testLogin.setBody("password", password);
+        testLogin.createEntity();
+        testLogin.request("POST", "/perform_login");
+        assertEquals(302, testLogin.responseCodeInt());
+        assertTrue(testLogin.responseCookieIndex(0).contains("JSESSIONID"));
+    }
+
+    @Test
+    public void testServeProfilePage() {
+        createRandomUserPass();
+        EscIntegrationSuite testNewUser = new EscIntegrationSuite(port, restTemplate);
+        testNewUser.setBody("username", username);
+        testNewUser.setBody("password", password);
+        testNewUser.setBody("phoneNumber", "555-555-5555");
+        testNewUser.createEntity();
+        testNewUser.request("POST", "/signup");
+        EscIntegrationSuite testProfile = new EscIntegrationSuite(port, restTemplate);
+        testProfile.setHeaders("Cookie", testNewUser.responseCookieIndex(0));
+        testProfile.resetBody();
+        testProfile.createEntity();
+        testProfile.resetResponse();
+        testProfile.request("GET", "/profile");
+        assertEquals(200, testProfile.responseCodeInt());
+        assertTrue(testProfile.responseBody().contains("<h1>PROFILE PAGE</h1>"));
+    }
 }
